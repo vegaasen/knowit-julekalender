e1d78860d289cb5ff7ce9d2caf1759733ee6e22d
vegaasen <vegaasen@gmail.com>
2014-12-18 23:18:16 +0100

    15 + 18.12.2014 :-)

---
+++ src/main/java/com/vegaasen/fun/julekalender/knowit/luke/DecEighteenth.java
@@ -0,0 +1,113 @@
+package com.vegaasen.fun.julekalender.knowit.luke;
+
+import com.google.common.base.Stopwatch;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Simple implementation using lambdas and conventional Java :-)
+ *
+ * @author <a href="vegaasen@gmail.com">vegardaasen</a>
+ */
+public class DecEighteenth {
+
+    private static final String WORDS = "words.txt";
+    private static final Map<String, Integer> ANAGRAMS = new HashMap<>();
+    private static final int INITIAL_VALUE = 1;
+
+    public static void main(String... args) {
+        doIt();
+    }
+
+    private static void doIt() {
+        try {
+            final URI words = ClassLoader.getSystemResource(WORDS).toURI();
+            doItWithLambda(words);
+            doItWithConventionalJava(words);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Wops.");
+        }
+    }
+
+    private static void doItWithLambda(URI words) throws IOException {
+        final Stopwatch casio = Stopwatch.createStarted();
+        Files
+                .readAllLines(Paths.get(words))
+                .stream()
+                .map(String::toLowerCase)
+                .map(String::toCharArray)
+                .map(candidate -> {
+                    Arrays.sort(candidate);
+                    return new String(candidate);
+                })
+                .collect(
+                        HashMap::new,
+                        (HashMap<String, Integer> map, String candidate) -> map.merge(
+                                candidate, 1, (what, counted) -> what + counted
+                        ),
+                        HashMap::putAll)
+                .entrySet()
+                .parallelStream().sorted((what, counted) -> counted.getValue().compareTo(what.getValue()))
+                .findFirst()
+                .ifPresent(result -> {
+                    casio.stop();
+                    System.out.println(String.format("Found {%s}. Using lambdas took {%sms}.", result, casio.elapsed(TimeUnit.MILLISECONDS)));
+                });
+    }
+
+    private static void doItWithConventionalJava(URI words) throws IOException {
+        final Stopwatch timex = Stopwatch.createStarted();
+        Files.readAllLines(Paths.get(words)).stream().forEach(DecEighteenth::checkAndPotentiallyAddAnagram);
+        final String mostSeen = filterResults();
+        timex.stop();
+        results(mostSeen, timex);
+    }
+
+    private static void checkAndPotentiallyAddAnagram(final String anagram) {
+        final String sortedAnagram = sortString(anagram);
+        if (ANAGRAMS.containsKey(sortedAnagram)) {
+            ANAGRAMS.put(sortedAnagram, ANAGRAMS.get(sortedAnagram) + 1);
+            return;
+        }
+        ANAGRAMS.put(sortedAnagram, INITIAL_VALUE);
+    }
+
+    private static String sortString(final String candidate) {
+        final char[] a = candidate.toLowerCase().toCharArray();
+        Arrays.sort(a);
+        return new String(a);
+    }
+
+    private static String filterResults() {
+        String mostSeen = "";
+        int times = 0;
+        for (final Map.Entry<String, Integer> anagram : ANAGRAMS.entrySet()) {
+            if (anagram.getValue() > times) {
+                times = anagram.getValue();
+                mostSeen = anagram.getKey();
+            }
+        }
+        ANAGRAMS.entrySet().parallelStream().filter(entry -> entry.getValue() >= 9).forEach(entry -> System.out.println(entry.getKey() + entry.getValue()));
+        return mostSeen;
+    }
+
+    private static void results(final String mostSeen, final Stopwatch stopwatch) {
+        System.out.println(String.format(
+                        "The {%s} is the one which we've seen the most with {%s} occurrences out of {%s} candidates. Using conventional Java took {%sms}",
+                        mostSeen,
+                        ANAGRAMS.get(mostSeen),
+                        ANAGRAMS.size(),
+                        stopwatch.elapsed(TimeUnit.MILLISECONDS)
+                )
+        );
+    }
+
+}
---
+++ src/main/java/com/vegaasen/fun/julekalender/knowit/luke/DecFifteen.java
@@ -0,0 +1,58 @@
+package com.vegaasen.fun.julekalender.knowit.luke;
+
+import com.google.common.base.Stopwatch;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author <a href="vegaasen@gmail.com">vegardaasen</a>
+ */
+public final class DecFifteen {
+
+    private static final int MAX = 9999, MIN = 1000;
+
+    public static void main(String... args) {
+        doIt();
+    }
+
+    private static void doIt() {
+        final Set<Integer> candidates = new HashSet<>(MAX - MIN);
+        final Stopwatch casio = Stopwatch.createStarted();
+        for (int n = MIN; n < MAX; n++) {
+            for (int x = 10; x <= 99; x++) {
+                for (int y = 99; y >= 10; y--) {
+                    if (x * y == n) {
+                        if (isLegalCandidate(x, y)) {
+                            candidates.add(n);
+                        }
+                    }
+                }
+            }
+        }
+        casio.stop();
+        printResults(candidates, casio);
+    }
+
+    private static boolean isLegalCandidate(final int... nums) {
+        boolean isDividable = false;
+        for (final int i : nums) {
+            if (i % 10 != 0) {
+                return true;
+            }
+            if (i % 10 == 0) {
+                isDividable = !isDividable;
+            }
+        }
+        return isDividable;
+    }
+
+    private static void printResults(final Set<Integer> what, final Stopwatch watch) {
+        what.parallelStream().forEach(System.out::println);
+        System.out.println(String.format("There were {%s} candidates. The process took {%sms}",
+                what.size(),
+                watch.elapsed(TimeUnit.MILLISECONDS)));
+    }
+
+}
