5b7da11c911b689d830a9b7d5d6e6a350377f10f
vegaasen <vegaasen@gmail.com>
2015-12-09 18:12:52 +0100

    9 solved :)

--- src/main/java/com/vegaasen/fun/julekalender/knowit/y2015/Ninth.java
+++ src/main/java/com/vegaasen/fun/julekalender/knowit/y2015/Ninth.java
@@ -1,15 +1,33 @@
 package com.vegaasen.fun.julekalender.knowit.y2015;
 
 import com.vegaasen.fun.julekalender.service.OJulMedDinGlede;
+import com.vegaasen.fun.julekalender.utils.MeasuredInvocation;
 
 /**
  * @author <a href="vegaasen@gmail.com">vegardaasen</a>
  */
 public class Ninth implements OJulMedDinGlede {
 
+    private static final int ALPHABET_LENGTH = 26;
+    private static final long EXCEL_COLUMN_LENGTH = 142453146368L;
+
     @Override
     public void hohoho() {
+        new MeasuredInvocation() {
+            @Override
+            public void what() {
+                System.out.println(String.format("What {%s}", findMeTheExcelColumnTitle(EXCEL_COLUMN_LENGTH)));
+            }
+        }.execute();
+    }
 
+    public static String findMeTheExcelColumnTitle(long columnLenght) {
+        StringBuilder excelColumnTitle = new StringBuilder();
+        while (columnLenght-- > 0) {
+            excelColumnTitle.append((char) ((int) 'A' + (columnLenght % ALPHABET_LENGTH)));
+            columnLenght /= ALPHABET_LENGTH;
+        }
+        return excelColumnTitle.reverse().toString();
     }
 
 }
